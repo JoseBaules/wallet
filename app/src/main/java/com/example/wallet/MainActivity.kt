b2d7e439804a55134f54fff6c551c78405eddcb7
JoseBaules <jvillarbaules@gmail.com>
2024-07-20 17:23:21 -0500

    More UI components

--- app/src/main/java/com/example/wallet/MainActivity.kt
+++ app/src/main/java/com/example/wallet/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.wallet
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class MainActivity : ComponentActivity() {
@@ -12,9 +14,18 @@ class MainActivity : ComponentActivity() {
 
         setContentView(R.layout.activity_main_menu)
 
-        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        val recyclerView: RecyclerView = findViewById(R.id.rv_documents)
         recyclerView.layoutManager = GridLayoutManager(this,2)
 //        recyclerView.adapter = DocumentAdapter(get.)
+
+        val fabAddDocument: FloatingActionButton =  findViewById(R.id.fab_add_document)
+
+        fabAddDocument.setOnClickListener{
+
+            //Intent to call addDocument
+
+
+        }
 
     }
 }
